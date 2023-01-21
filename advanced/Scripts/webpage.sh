acb0762beefe59cad85e26ed8153479cab0e3dad
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-21 07:04:40 -0500

    switch to tmux

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -603,19 +603,19 @@ EOF'
 }
 
 UpdateSpeedTest() {
-    if ! command -v screen &> /dev/null
-    then
-        apt-get install screen -y
+    if ! command -v tmux &> /dev/null; then
+        apt-get install tmux -y
     fi
-    screen -S pimod -dm bash -c "curl -sSL https://github.com/arevindh/pihole-speedtest/raw/master/mod.sh | bash -s -- up ${args[2]}"
+    tmux new-session -d -s pimod
+    tmux send-keys -t pimod "curl -sSL https://github.com/arevindh/pihole-speedtest/raw/master/mod.sh | tac | tac | bash -s -- up ${args[2]}" Enter
 }
 
 UninstallSpeedTest() {
-    if ! command -v screen &> /dev/null
-    then
-        apt-get install screen -y
+    if ! command -v tmux &> /dev/null; then
+        apt-get install tmux -y
     fi
-    screen -S pimod -dm bash -c "curl -sSL https://github.com/arevindh/pihole-speedtest/raw/master/mod.sh | bash -s -- un"
+    tmux new-session -d -s pimod
+    tmux send-keys -t pimod "curl -sSL https://github.com/arevindh/pihole-speedtest/raw/master/mod.sh | tac | tac | bash -s -- un" Enter
 }
 
 SetWebUITheme() {
