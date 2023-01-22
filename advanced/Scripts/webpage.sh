562c44aaba241f05f20b3627a9aac84b6424d7f6
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-22 12:56:17 -0500

    persist tmux again

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -607,14 +607,18 @@ UpdateSpeedTest() {
     if ! command -v tmux &> /dev/null; then
         apt-get install tmux -y
     fi
-    tmux new-session -d -s pimod "curl -sSLN https://github.com/ipitio/pihole-speedtest/raw/ipitio/mod.sh | sudo bash -s -- up ${args[2]}"
+    tmux kill-session -t pimod &> /dev/null
+    tmux new-session -d -s pimod
+    tmux send-keys -t pimod "curl -sSLN https://github.com/ipitio/pihole-speedtest/raw/ipitio/mod.sh | sudo bash -s -- up ${args[2]}"
 }
 
 UninstallSpeedTest() {
     if ! command -v tmux &> /dev/null; then
         apt-get install tmux -y
     fi
-    tmux new-session -d -s pimod "curl -sSLN https://github.com/ipitio/pihole-speedtest/raw/ipitio/mod.sh | sudo bash -s -- un"
+    tmux kill-session -t pimod &> /dev/null
+    tmux new-session -d -s pimod 
+    tmux send-keys -t pimod "curl -sSLN https://github.com/ipitio/pihole-speedtest/raw/ipitio/mod.sh | sudo bash -s -- un"
 }
 
 SetWebUITheme() {
