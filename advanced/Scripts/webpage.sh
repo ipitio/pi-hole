9b912eaa928c65e7c09c8aeb8537232f1141a0aa
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-29 08:19:00 -0500

    single quotes really?

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -560,7 +560,7 @@ SetService() {
     if [[ "$1" == "0" ]]; then
         systemctl disable --now pihole-speedtest.timer &> /dev/null
     else
-        speedtestfile="/var/www/html/admin/scripts/pi-hole/speedtest/speedtest-official.sh"
+        speedtestfile='/var/www/html/admin/scripts/pi-hole/speedtest/speedtest-official.sh'
         sudo bash -c 'cat > /etc/systemd/system/pihole-speedtest.service << EOF
 [Unit]
 Description=Pi-hole Speedtest
