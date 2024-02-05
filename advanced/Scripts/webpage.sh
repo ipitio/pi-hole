db9c82fcc364171a9e868e21dc0aca58235f49ce
ipitio <21136719+ipitio@users.noreply.github.com>
2024-02-05 08:54:39 -0500

    use db for time

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -605,22 +605,24 @@ generate_cron_schedule() {
 last_run_file="/var/tmp/last_run_time"
 interval_seconds='"$total_seconds"'
 
-if [[ -f "\$last_run_file" ]]; then
-    last_run=\$(cat "\$last_run_file")
-    current_time=\$(date +%s)
-    if (( current_time - last_run < interval_seconds )); then
-        exit 0
-    fi
+last_run_time=\$(sqlite3 /etc/pihole/speedtest.db "SELECT start_time FROM speedtest ORDER BY start_time DESC LIMIT 1;")
+start_time=\$(date -d "\$last_run_time" +%s)
+current_time=\$(date +%s)
+if (( current_time - start_time < interval_seconds )); then
+    exit 0
 fi
 
-echo \$(date +%s) > "\$last_run_file"
-# Execute the actual job
 /bin/bash /opt/pihole/speedtestmod/speedtest.sh
 EOF'
     sudo chmod +x "$schedule_script"
 
     crontab -l | grep -v "$schedule_script" | crontab -
-    (crontab -l; echo "* * * * * $schedule_script") | crontab -
+    echo "* * * * * $schedule_script" | EDITOR='tee -a' crontab -u root -e
+
+    # if the table is empty, run the speedtest now
+    if [[ -z $(sqlite3 /etc/pihole/speedtest.db "SELECT start_time FROM speedtest ORDER BY start_time DESC LIMIT 1;") ]]; then
+        /bin/bash /opt/pihole/speedtestmod/speedtest.sh
+    fi
 }
 
 get_scheduler() {
