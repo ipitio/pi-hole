c8dac929f3a2dfefad107e09c617ac263511af89
ipitio <21136719+ipitio@users.noreply.github.com>
2024-02-06 03:14:21 -0500

    always run tab

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -640,10 +640,8 @@ EOF
     sudo chmod +x "$schedule_script"
 
     crontab -l 2>/dev/null | grep -v "$schedule_script" | crontab -
-    if (( $(echo "$total_seconds > 0" | bc -l) )); then
-        crontab -l &> /dev/null || crontab -l 2>/dev/null | { cat; echo ""; } | crontab -
-        (crontab -l 2>/dev/null; echo "* * * * * /bin/bash $schedule_script") | crontab -
-    fi
+    crontab -l &> /dev/null || crontab -l 2>/dev/null | { cat; echo ""; } | crontab -
+    (crontab -l 2>/dev/null; echo "* * * * * /bin/bash $schedule_script") | crontab -
 }
 
 get_scheduler() {
