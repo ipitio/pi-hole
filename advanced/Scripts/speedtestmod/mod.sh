e86fa90a59f2e88357d6590071d62aa9a7045f19
ipitio <21136719+ipitio@users.noreply.github.com>
2024-04-16 05:47:56 -0400

    one less if

--- advanced/Scripts/speedtestmod/mod.sh
+++ advanced/Scripts/speedtestmod/mod.sh
@@ -82,11 +82,8 @@ download() {
         grep -q "$desiredVersion$" <<<"$tags" && desiredVersion=$(grep "$desiredVersion$" <<<"$tags" | awk '{print $1;}') || desiredVersion=$currentVersion
     fi
 
-    if [ "$currentVersion" != "$desiredVersion" ]; then
-        git fetch origin --depth=1 $desiredVersion -q
-        git -c advice.detachedHead=false checkout $desiredVersion -q
-    fi
-
+    git fetch origin --depth=1 $desiredVersion -q
+    git -c advice.detachedHead=false checkout $desiredVersion -q
     cd ..
 }
 
